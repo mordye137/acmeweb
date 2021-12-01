50035a9bd7a264d957544569f5e30b17f9e85c79
mordye137 <mordymepstein@gmail.com>
2021-12-01 13:28:18 -0500

    Created MockSystemDetails that implements the IDetails interface and returns mock values. Added javadocs.

---
+++ src/main/java/com/acme/detailed/MockSystemDetails.java
@@ -0,0 +1,43 @@
+package com.acme.detailed;
+
+public class MockSystemDetails implements IDetails{
+    /**
+     * @return mock available processors
+     */
+    @Override
+    public int getAvailableProcessors() {
+        return 4;
+    }
+
+    /**
+     * @return mock free JVM memory
+     */
+    @Override
+    public long getFreeJVMMemory() {
+        return 127268272;
+    }
+
+    /**
+     * @return mock total JVM memory
+     */
+    @Override
+    public long getTotalJVMMemory() {
+        return 159383552;
+    }
+
+    /**
+     * @return mock jre version
+     */
+    @Override
+    public String getJreVersion() {
+        return "15.0.2+7-27";
+    }
+
+    /**
+     * @return mock temp location
+     */
+    @Override
+    public String getTempLocation() {
+        return "M:\\\\AppData\\\\Local\\\\Temp";
+    }
+}
