3bbc8ef34198f02f3461c97d081c5d11f721e919
mordye137 <mordymepstein@gmail.com>
2021-12-01 13:25:28 -0500

    Created facade that implements the IDetails interface and returns values from Runtime and System. Added javadocs.

--- src/main/java/com/acme/detailed/IDetails.java
+++ src/main/java/com/acme/detailed/IDetails.java
@@ -1,14 +1,33 @@
 package com.acme.detailed;
 
+
+/**
+ * This interface is made to return information about the System
+ */
 public interface IDetails {
 
+    /**
+     * @return available processors
+     */
     int getAvailableProcessors ();
 
+    /**
+     * @return free JVM memory
+     */
     long getFreeJVMMemory ();
 
+    /**
+     * @return total JVM memory
+     */
     long getTotalJVMMemory ();
 
+    /**
+     * @return jre version
+     */
     String getJreVersion ();
 
+    /**
+     * @return temp location
+     */
     String getTempLocation ();
 }
---
+++ src/main/java/com/acme/detailed/SystemDetails.java
@@ -0,0 +1,46 @@
+package com.acme.detailed;
+
+public class SystemDetails implements IDetails{
+
+    private Runtime runtime;
+
+    /**
+     * @return available processors from runtime
+     */
+    @Override
+    public int getAvailableProcessors() {
+        return runtime.availableProcessors();
+    }
+
+    /**
+     * @return free JVM memory from runtime
+     */
+    @Override
+    public long getFreeJVMMemory() {
+        return runtime.freeMemory();
+    }
+
+    /**
+     * @return total JVM memory from runtime
+     */
+    @Override
+    public long getTotalJVMMemory() {
+        return runtime.totalMemory();
+    }
+
+    /**
+     * @return jre version from System properties
+     */
+    @Override
+    public String getJreVersion() {
+        return System.getProperty("java.runtime.version");
+    }
+
+    /**
+     * @return temp location from System
+     */
+    @Override
+    public String getTempLocation() {
+        return System.getenv("TEMP");
+    }
+}
